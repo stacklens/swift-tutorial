40668f775f457015c47ef297606c8421be5f6d46
Dusai <474377974@qq.com>
2022-05-04 20:59:30 +0800

    todolist

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var todoList = [
+    ToDoItem(name: "Apple"),
+    ToDoItem(name: "Pear"),
+    ToDoItem(name: "Tomato")
+  ]
+  
+  @State private var newName: String = ""
+  
+  var body: some View {
+    VStack {
+      HStack {
+        TextField("输入新事项", text: $newName)
+        Button("确认") {
+          let newItem = ToDoItem(name: newName)
+          todoList.append(newItem)
+          newName = ""
+        }
+      }
+      .padding()
+      
+      List {
+        ForEach(todoList) { item in
+          HStack {
+            Toggle("", isOn: item.$isOn)
+              .toggleStyle(.checkbox)
+            Text(item.name)
+
+          }
+        }
+      }
     }
+  }
 }
 
+struct ToDoItem: Identifiable {
+  let id    = UUID()
+  @Binding var isOn: Bool
+  let name: String
+  
+  init(name: String) {
+    self.name = name
+    self._isOn = Binding.constant(true)
+  }
+}
+
+
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
