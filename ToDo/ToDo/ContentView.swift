7bae9c36e577feedc6d0f6cf2d4e9b9c6867d502
Dusai <474377974@qq.com>
2022-05-05 22:11:07 +0800

    todo List

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -8,11 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var todoList = [
-    ToDoItem(name: "Apple"),
-    ToDoItem(name: "Pear"),
-    ToDoItem(name: "Tomato")
-  ]
+  @StateObject private var viewModel = ToDoViewModel()
   
   @State private var newName: String = ""
   
@@ -22,19 +18,33 @@ struct ContentView: View {
         TextField("输入新事项", text: $newName)
         Button("确认") {
           let newItem = ToDoItem(name: newName)
-          todoList.append(newItem)
+          viewModel.append(newItem)
           newName = ""
         }
       }
       .padding()
       
       List {
-        ForEach(todoList) { item in
+        ForEach(viewModel.todoList) { item in
           HStack {
-            Toggle("", isOn: item.$isOn)
-              .toggleStyle(.checkbox)
             Text(item.name)
+              .foregroundColor(item.isOn ? .primary : .gray)
+            
+            Spacer()
+            
+            Group {
+              if item.isOn {
+                Image(systemName: "circle")
 
+              }
+              else {
+                Image(systemName: "checkmark.circle.fill")
+              }
+            }
+              .foregroundColor(.blue)
+              .onTapGesture {
+                viewModel.toggle(item)
+              }
           }
         }
       }
@@ -42,15 +52,32 @@ struct ContentView: View {
   }
 }
 
+class ToDoViewModel: ObservableObject {
+  @Published private(set) var todoList: [ToDoItem]
+  
+  init() {
+    self.todoList = [
+      ToDoItem(name: "Apple"),
+      ToDoItem(name: "Pear"),
+      ToDoItem(name: "Tomato")
+    ]
+  }
+  
+  func append(_ item: ToDoItem) {
+    todoList.append(item)
+  }
+  
+  func toggle(_ item: ToDoItem) {
+    if let index = todoList.firstIndex(where: {$0.id == item.id}) {
+      todoList[index].isOn.toggle()
+    }
+  }
+}
+
 struct ToDoItem: Identifiable {
   let id    = UUID()
-  @Binding var isOn: Bool
+  var isOn  = true
   let name: String
-  
-  init(name: String) {
-    self.name = name
-    self._isOn = Binding.constant(true)
-  }
 }
 
 
